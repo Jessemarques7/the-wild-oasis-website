f01bb3f9e673ed65c7379cbcad9e960e9bb6a945
Jesse Marques <jessemateus919@gmail.com>
2024-12-27 13:29:53 -0300

    main pages

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <Image
+      {/* <Image
         priority
         src={"/logo.png"}
+        height="60"
+        width="60"
+        alt="The Wild Oasis logo"
+      /> */}
+      <Image
+        priority
+        src={logo}
+        quality={100}
         height="60"
         width="60"
         alt="The Wild Oasis logo"
