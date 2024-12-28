9fb15fd24e3bbdc0ee529b3accdeda4c24be49e9
Jesse Marques <jessemateus919@gmail.com>
2024-12-28 13:22:24 -0300

    setup part

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,5 +1,9 @@
 function page() {
-  return <div></div>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, user
+    </h2>
+  );
 }
 
 export default page;
