c0da62b8a36d8cff31b4e29313b62f914f3b69b6
Jesse Marques <jessemateus919@gmail.com>
2025-01-05 20:30:59 -0300

    Add reservation functionality with context; implement cabin filtering and reminder components

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,10 +1,14 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -19,8 +23,13 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
